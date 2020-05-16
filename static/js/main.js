278958b6b02996c9dca045da07ee676ebfef1fe9
JangGeonil <>
2020-05-17 01:07:59 +0900

    nav

---
+++ static/js/main.js
@@ -0,0 +1,26 @@
+'use strict';
+(() => {
+    //Make navbar transparent when it is on the top
+    const navbar = document.querySelector('#navbar');
+    const navbarHeight = navbar.getBoundingClientRect().height;
+    document.addEventListener('scroll', () => {
+        if (window.scrollY > navbarHeight) {
+            navbar.classList.add('navbar--dark');
+        } else {
+            navbar.classList.remove('navbar--dark');
+        }
+    });
+
+
+    //Handle scrolling when tapping on the navbar menu
+    const navbarMenu = document.querySelector('.navbar__menu');
+    navbarMenu.addEventListener('click', (e) => {
+        const target = e.target;
+        const link = target.dataset.link;
+        if (link == null) {
+            return
+        }
+        const scrollTo = document.querySelector(link);
+        scrollTo.scrollIntoView({ behavior: 'smooth' });
+    });
+})();
