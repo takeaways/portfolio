58cedc897e1ea93a3e0779554e3dc2374b506f19
JangGeonil <>
2020-05-24 10:13:56 +0900

    8320

--- init.js
+++ init.js
@@ -2,11 +2,11 @@ const app = require("./app");
 const dbSync = require("./utile/db");
 const dotenv = require('dotenv');
 dotenv.config();
-
+const PORT = 8320
 dbSync().then(() => {
     console.log('Sync Database..');
-    app.listen(4333, () => {
-        console.log("server start port 4333");
+    app.listen(PORT, () => {
+        console.log("server start port ," + PORT);
     });
 });
 
